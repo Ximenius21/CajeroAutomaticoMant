95f5f755ae138979ac21b5d67f8dd5f458f2e078
Ximenius21 <ximenalopezhdez209@gmail.com>
2024-05-27 22:35:17 -0700

    Commit prueba

--- Consulta.java
+++ Consulta.java
@@ -12,5 +12,6 @@ public class Consulta extends Clase_Abstractaa{
         System.out.println("Se relalizo una Consulta el 31/10/2023");
         System.out.println("Tu saldo actual es: " + getSaldo());
         System.out.println("====================================");
+        System.out.println("PRUEBA q")
     }    
 }
